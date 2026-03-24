a66913c52c6a64c97e3169431f3d1765438c64fd
chesyannisa <chesya.aizzati@gmail.com>
2026-03-24 07:29:32 +0700

    main styling + navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
-import {NavLink} from "react-router-dom"
-import styles from "../styles/Navbar.module.css"
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Navbar.module.css";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const user= useSelector((state)=> state.auth.user)
+  const user = useSelector((state) => state.auth.user);
+
   return (
-    <nav>
-      <ul className={styles.navWrapper}>
-{user?.name && <li>Welcome {user.name}</li>}
-        <li>
-          <NavLink to={"/"}>Movies</NavLink>
-        </li>
-        <li>
-          <NavLink to={"login"}>Login</NavLink>
-        </li>
-      </ul>
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>MovieCenter</h1>
+
+        <ul className={styles.navWrapper}>
+          {user?.name && <li className={styles.welcome}>Hi, {user.name}</li>}
+
+          <li>
+            <NavLink to="/">Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
